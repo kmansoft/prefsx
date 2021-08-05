a07a78e63d9259ca251b0ff117017ad6b565dacb
Kostya Vasilyev <kmansoft@gmail.com>
2021-08-05 22:06:40 +0300

    Add constructors

--- src/main/java/org/kman/prefsx/DialogPreferenceX.kt
+++ src/main/java/org/kman/prefsx/DialogPreferenceX.kt
@@ -4,9 +4,32 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.fragment.app.DialogFragment
 import androidx.preference.DialogPreference
+import androidx.preference.R
 
-abstract class DialogPreferenceX(context: Context, attrs: AttributeSet?)
-	: DialogPreference(context, attrs) {
+abstract class DialogPreferenceX(
+	context: Context,
+	attrs: AttributeSet?,
+	defStyleAttr: Int,
+	defStyleRes: Int
+) : DialogPreference(context, attrs, defStyleAttr, defStyleRes) {
 
-	abstract fun createDialogFragment() : DialogFragment?
+	constructor(
+		context: Context,
+		attrs: AttributeSet?,
+		defStyleAttr: Int
+	) : this(context, attrs, defStyleAttr, 0) {
+	}
+
+	constructor(
+		context: Context,
+		attrs: AttributeSet?
+	) : this(
+		context, attrs, TypedArrayUtils.getAttr(
+			context, R.attr.dialogPreferenceStyle,
+			android.R.attr.dialogPreferenceStyle
+		), 0
+	) {
+	}
+
+	abstract fun createDialogFragment(): DialogFragment?
 }
